d385ee1f9cd39ae329702383754ee1a66f562c9e
AdiGo22 <coderadigo@gmail.com>
2025-04-19 00:25:37 +0530

    Added search page features with debouncing results handling errors and irrelevant responses

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,6 +1,8 @@
-import { icons } from "@/constants/icons";
 import { Link } from "expo-router";
 import { Text, Image, TouchableOpacity, View } from "react-native";
+
+import { icons } from "@/constants/icons";
+
 const MovieCard = ({
   id,
   poster_path,
@@ -25,21 +27,24 @@ const MovieCard = ({
           {title}
         </Text>
 
-        <View
-        className="flex-row items-center justify-start gap-x-1">
-            <Image source = {icons.star} 
-             className="size-4"/>
-             <Text className="text-xs font-bold uppercase text-white">{Math.round(vote_average/2)}</Text>
-            </View>
+        <View className="flex-row items-center justify-start gap-x-1">
+          <Image source={icons.star} className="size-4" />
+          <Text className="text-xs text-white font-bold uppercase">
+            {Math.round(vote_average / 2)}
+          </Text>
+        </View>
 
-            <View className="flex-row items-center justify-between">
-                <Text className="text-xs text-light-300 font-medium mt-1">{release_date?.split('-')[0]}</Text>
-                <Text className="text-xs font-medium text-light-300 uppercase">Movie</Text>
-            </View>
-
-        </TouchableOpacity>
-        </Link>
+        <View className="flex-row items-center justify-between">
+          <Text className="text-xs text-light-300 font-medium mt-1">
+            {release_date?.split("-")[0]}
+          </Text>
+          <Text className="text-xs font-medium text-light-300 uppercase">
+            Movie
+          </Text>
+        </View>
+      </TouchableOpacity>
+    </Link>
   );
-}
+};
 
 export default MovieCard;
